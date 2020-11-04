45c1d7b1e32e9b25ea9ce2134ba03e2df403850e
Eugene Tulushev <eugene@tulushev.com>
2020-11-04 21:52:38 +0200

    Excluding templates from the package's product

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   targets: [
     .target(
       name: "Prelude",
-      dependencies: []
+      exclude: ["Templates"]
     ),
     .testTarget(
       name: "PreludeTests",
