e1db277667c643ea5ce1dc75a91772653e85c6ec
Eugene Tulushev <eugene@tulushev.com>
2020-11-13 09:52:13 +0200

    Add a nested codable container for These both case

--- Sources/Prelude/These.swift
+++ Sources/Prelude/These.swift
@@ -15,27 +15,48 @@ extension These: Comparable where A: Comparable, B: Comparable {}
 // MARK: - Codable
 
 extension These: Codable where A: Codable, B: Codable {
+  enum CodingKeys: CodingKey {
+    case this, that, both
+  }
+  
   public init(from decoder: Decoder) throws {
-    do {
-      self = try .both(.init(from: decoder), .init(from: decoder))
-    } catch {
-      do {
-        self = try .this(.init(from: decoder))
-      } catch {
-        self = try .that(.init(from: decoder))
-      }
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let key = container.allKeys.first
+    
+    switch key {
+    case .none:
+      throw DecodingError.dataCorrupted(
+        DecodingError.Context(
+          codingPath: container.codingPath,
+          debugDescription: "Unable to decode These"
+        )
+      )
+    case .some(.this):
+      let a = try container.decode(A.self, forKey: .this)
+      self = .this(a)
+    case .some(.that):
+      let b = try container.decode(B.self, forKey: .that)
+      self = .that(b)
+    case .some(.both):
+      var nestedContainer = try container.nestedUnkeyedContainer(forKey: .both)
+      let a = try nestedContainer.decode(A.self)
+      let b = try nestedContainer.decode(B.self)
+      self = .both(a, b)
     }
   }
   
   public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    
     switch self {
     case let .this(a):
-      try a.encode(to: encoder)
+      try container.encode(a, forKey: .this)
     case let .that(b):
-      try b.encode(to: encoder)
+      try container.encode(b, forKey: .that)
     case let .both(a, b):
-      try a.encode(to: encoder)
-      try b.encode(to: encoder)
+      var nestedContainer = container.nestedUnkeyedContainer(forKey: .both)
+      try nestedContainer.encode(a)
+      try nestedContainer.encode(b)
     }
   }
 }
